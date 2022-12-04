af23204be233d543f450a811c0e0dba962a248a1
billtrancon12 <69377326+billtrancon12@users.noreply.github.com>
2022-12-04 16:28:59 -0600

    added some create account instructions

--- scheduler_project/src/components/createAccountForm.js
+++ scheduler_project/src/components/createAccountForm.js
@@ -3,9 +3,21 @@ import Input from './Input'
 import InfoIcon from '@mui/icons-material/Info';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types'
+import { toast, ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+
 
 export default class CreateAccountForm extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      usernameInstruction: [],
+      passwordInstruction: []
+    }
+  }
   render() {
+    console.log(this.state.usernameInstruction);
     return (
       <form className='signup_form' onSubmit={this.props.onSubmit}>
         <div className='email_signup_wrapper signup_input_wrapper'>
@@ -13,11 +25,11 @@ export default class CreateAccountForm extends Component {
         </div>
         <div className='username_signup_wrapper signup_input_wrapper'>
           <Input className="username_signup input niceText" type="text" placeholder="Enter your username" name="username" onChange={this.props.onChange}></Input>
-          <InfoIcon></InfoIcon>
+          <InfoIcon onClick={() => this.showUsernameInstructions()}></InfoIcon>{this.state.usernameInstruction}
         </div>
         <div className='password_signup_wrapper signup_input_wrapper'>
           <Input className="password_signup input niceText" type="password" placeholder="Enter your password" name="password" onChange={this.props.onChange}></Input>
-          <InfoIcon></InfoIcon>
+          <InfoIcon onClick={() => this.showPasswordInstruction()}></InfoIcon>{this.state.passwordInstruction}
         </div>
         <div className='retype_pass_signup_wrapper signup_input_wrapper'>
           <Input className="retype_pass_signup input niceText" type="password" placeholder="Retype your password" name="retype_pass" onChange={this.props.onChange}></Input>
@@ -29,6 +41,48 @@ export default class CreateAccountForm extends Component {
           <button type="submit" className='signup_button button'>Create</button>
         </div>
       </form>
+    )
+  }
+
+  showUsernameInstructions(){
+    const toasted = document.getElementsByClassName("Toastify")[0];
+    if(toasted === undefined || toasted === null || toasted.childElementCount === 0){
+      toast(this.getUsernameInstruction(), {position: 'top-center', style: {marginTop: "250px"}});
+      this.setState({usernameInstruction: <ToastContainer autoClose={3000} className="niceText" rows={3}></ToastContainer>})
+    }
+  }
+
+  showPasswordInstruction(){
+    const toasted = document.getElementsByClassName("Toastify")[0];
+    if(toasted === undefined || toasted === null || toasted.childElementCount === 0){
+      toast(this.getPasswordInstruction(), {position: 'top-center', style: {marginTop: "250px"}});
+      this.setState({passwordInstruction: <ToastContainer autoClose={3000} className="niceText" rows={3}></ToastContainer>})
+    }
+  }
+  getUsernameInstruction(){
+    return(
+      <span className='username_instruction niceText'>
+        1. Must be 8-12 characters long
+        <br></br>
+        2. Cannot have special characters
+      </span>
+    )
+  }
+
+  getPasswordInstruction(){
+    return(
+      <span className='password_instruction niceText'>
+        1. Must be 8-20 characters long
+        <br></br>
+        2. Must have at least one special characters 
+        including '?', '@', '!', '#', '$', '%', '^', '&'
+        <br></br>
+        3. Must have at least one number
+        <br></br>
+        4. Must have at least one lowercase character
+        <br></br>
+        5. Must have at least one uppercase character
+      </span>
     )
   }
 }
